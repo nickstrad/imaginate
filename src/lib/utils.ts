244d40794f778df9287980711507ca0e90553ec8
Nick Strad <nickstrad.dev@gmail.com>
2026-04-19 14:36:05 -0500

    remove inngest agent-kit utilities
    Clean up parseAgentOutput and old import from migration to vercel ai sdk.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,23 +1,6 @@
-import { Message } from "@inngest/agent-kit";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export const parseAgentOutput = (
-  generatorOutput: Message[],
-  defaultMessage: string
-): string => {
-  const output = generatorOutput[0];
-  if (output.type !== "text") {
-    return defaultMessage;
-  }
-
-  if (Array.isArray(output.content)) {
-    return output.content.map((t) => t).join("");
-  }
-
-  return output.content;
-};
