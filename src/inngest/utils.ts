db9f8ef5201c5c6d2a474d053c2e769629a6ef95
Nick Strad <nickstrad.dev@gmail.com>
2026-04-21 03:14:04 -0500

    extend sandbox timeout to 30 minutes

--- src/inngest/utils.ts
+++ src/inngest/utils.ts
@@ -1,6 +1,6 @@
 import { Sandbox } from "@e2b/code-interpreter";
 
-export const SANDBOX_TIMEOUT = 5 * 60 * 1000; // 10 minutes
+export const SANDBOX_TIMEOUT = 30 * 60 * 1000; // 30 minutes
 export const getSandbox = async (sandboxId: string) => {
   const sandbox = await Sandbox.connect(sandboxId);
   await sandbox.setTimeout(SANDBOX_TIMEOUT);
